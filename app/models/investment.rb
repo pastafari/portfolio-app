c2b8bf3adf4319e9fa2f2c51ddcb2fefa616798b
Mohit Thatte <mohit.thatte@gmail.com>
2012-09-16 15:21:47 +0530

    Last commit

--- app/models/investment.rb
+++ app/models/investment.rb
@@ -1,3 +1,12 @@
 class Investment < ActiveRecord::Base
   attr_accessible :company_id, :cost, :portfolio_id, :quantity
+
+  validate :quantity_should_be_positive
+  
+  def quantity_should_be_positive
+    if quantity < 0
+      errors.add :quantity, 'should be positive'
+    end
+  end
+  
 end
